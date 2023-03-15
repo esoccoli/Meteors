36a5b27052e2e70e7ecf095f737cb3eb189ea261
Ella Soccoli <soccoli2015@gmail.com>
2023-03-15 14:57:20 -0400

    Added more comments and made S key work as an alternative to W key

--- Player.cs
+++ Player.cs
@@ -7,19 +7,32 @@ using static Devcade.Input;
 
 namespace Meteors;
 
+/// <summary>
+/// Manages the player controllable spaceship
+/// </summary>
 public class Player
 {
     private Texture2D texture;
     private Vector2 position;
     private double angle;
     
+    /// <summary>
+    /// Provides the needed information to create a player
+    /// </summary>
+    /// <param name="texture">Texture file for the ship</param>
+    /// <param name="position">Position vector of the player</param>
     public Player(Texture2D texture, Vector2 position)
     {
         this.texture = texture;
         this.position = position;
         angle = Math.PI / 2;
     }
-
+    
+    /// <summary>
+    /// Updates the player's position and rotation each frame based on keybord/devcade input
+    /// Currently the ship moves at constant velocity, will implement acceleration later
+    /// </summary>
+    /// <param name="gameTime">Elapsed time in the game since the last frame</param>
     public void Update(GameTime gameTime)
     {
         // Rotates the ship counterclockwise if the A button is pressed or if the joystick is moved left
@@ -38,8 +51,9 @@ public class Player
             angle += 0.05;
         }
 
-        // Moves the ship forward if the W button is pressed or if the joystick is moved up
+        // Moves the ship forward if the W or S key is pressed or if the joystick is moved up
         if (Keyboard.GetState().IsKeyDown(Keys.W) ||
+            Keyboard.GetState().IsKeyDown(Keys.S) ||
             GetButtonDown(1, ArcadeButtons.StickUp) ||
             GetButtonDown(2, ArcadeButtons.StickUp))
         {
@@ -49,6 +63,10 @@ public class Player
         }
     }
     
+    /// <summary>
+    /// Draws the player ship at the proper position and rotation
+    /// </summary>
+    /// <param name="spriteBatch">Object that allows items to be drawn to the screen</param>
     public void Draw(SpriteBatch spriteBatch)
     {
         spriteBatch.Draw(texture, position, null, Color.White, (float)angle, new Vector2(texture.Width / 2, texture.Height / 2), 1f, SpriteEffects.None, 0f);
